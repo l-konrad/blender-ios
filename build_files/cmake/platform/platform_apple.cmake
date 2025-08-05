6a496b089701ab6f20e01566d67e9a6e07924382
Jonas Holzman <jonas@holzman.fr>
2025-08-06 00:12:02 +0200

    iOS: Add iPad Simulator Support
    This PR brings support for the Xcode iPad iOS Simulator. As the iOS
    Simulator Framework, SDK and ABI are different, this patch effectively
    adds a new separate `ios-simulator` platform, and requires a separate
    set of library dependencies to be built. This required the following
    changes to the build system:
    
    - Add a new `ios-simulator` `APPLE_TARGET_DEVICE`, with the required
      SDK / compile flag changes
    - Add a new `APPLE_TARGET_IOS` CMake helper variable which is `TRUE` for
      both iOS and iOS Simulator builds, used for general iOS checks
    - Add a new `make deps ios-simulator` command to invoke the dependency
      builder, which will be built in `../build_ios-simulator/arm64/` and
      installed in `/lib/ios-simulator_arm64/`
    
    The dependency builder also required the following iOS library patches
    modifications:
    - FFmpeg: Add a new `ios-simulator` OS target, which mostly take the
      same setting as its `ios` counterpart with a compilation flag
      modification to use the right ABI, patch was also slightly
      cleaned-up
    - x265: Use the `APPLE_OS_MINVERSION_CFLAG` CMake variable to use the
      right compilation flag, removing a TODO
    
    Since separate libraries need to be built in
    `/lib/ios-simulator_arm64/`, this directory was added to the
    `.gitignore` to keep a clean git working tree. Since this platform is
    only meant to be used by developers, I didn't see any need to make it a
    precompiled library submodule like other platforms, but this could
    eventually be reconsidered, let me know what you think.
    
    See the Pull Request for details on how to build and run Blender on the
    Simulator
    
    Pull Request: https://projects.blender.org/blender/blender/pulls/143389

--- build_files/cmake/platform/platform_apple.cmake
+++ build_files/cmake/platform/platform_apple.cmake
@@ -83,13 +83,14 @@ if(WITH_APPLE_CROSSPLATFORM)
   # That might still be a better option though. See "debug_size_" in BLI_vector.hh for an example of this.
   set(CMAKE_TOOLS_ARGS "${CMAKE_TOOLS_ARGS} -DCMAKE_C_FLAGS=\"-DWITH_CROSSCOMPILED_TOOLS -DWITH_APPLE_CROSSPLATFORM\"")
   set(CMAKE_TOOLS_ARGS "${CMAKE_TOOLS_ARGS} -DCMAKE_CXX_FLAGS=\"-DWITH_CROSSCOMPILED_TOOLS -DWITH_APPLE_CROSSPLATFORM\"")
-  
+
+  get_filename_component(CMAKE_BIN_DIRECTORY "${CMAKE_COMMAND}" DIRECTORY)
   add_custom_target(blender_cross_tools_compile
     COMMENT "\n---------------------------\n Building Cross Compile Tools\n"
-    COMMAND cd ${CMAKE_SOURCE_DIR}
-    COMMAND env -i BUILD_CMAKE_ARGS=${CMAKE_TOOLS_ARGS} BUILD_DIR=${CROSSCOMPILE_TOOLDIR} make tools
+    COMMAND env -i PATH="${CMAKE_BIN_DIRECTORY}:$ENV{PATH}" BUILD_CMAKE_ARGS=${CMAKE_TOOLS_ARGS} BUILD_DIR=${CROSSCOMPILE_TOOLDIR} make tools
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
   )
-  
+
   # Configure executable dependencies and paths for cross-compiled tools.
   # NOTE: Local dependency creation e.g. makesdna should check WITH_CROSSCOMPILED_TOOLS before creating local target.
   #       We instead run a full build to generate the targets in a host-side build on macOS.
@@ -754,11 +755,13 @@ endif()
 # set(CMAKE_XCODE_ATTRIBUTE_PRODUCT_BUNDLE_IDENTIFIER "org.blenderfoundation.blender")
 
 if(WITH_APPLE_CROSSPLATFORM)
-  if(APPLE_TARGET_DEVICE STREQUAL "ios")
+  if(APPLE_TARGET_IOS)
     set(CMAKE_XCODE_ATTRIBUTE_TARGETED_DEVICE_FAMILY "1,2")
     set(CMAKE_XCODE_ATTRIBUTE_SUPPORTS_MACCATALYST NO)
     set(CMAKE_XCODE_ATTRIBUTE_SUPPORTS_MAC_DESIGNED_FOR_IPHONE_IPAD NO)
+
+    # Entitlements file reference
+    # `release/ios` is hardcoded since we want to use the same entitlements for both iOS-Simulator and normal iOS builds.
+    set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_ENTITLEMENTS "${CMAKE_SOURCE_DIR}/release/ios/entitlements.plist")
   endif()
-  # Entitlements file reference
-  set(CMAKE_XCODE_ATTRIBUTE_CODE_SIGN_ENTITLEMENTS "${CMAKE_SOURCE_DIR}/release/${APPLE_TARGET_DEVICE}/entitlements.plist")
 endif()
