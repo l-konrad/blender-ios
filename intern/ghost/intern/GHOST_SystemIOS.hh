6734190e7e814f628f7b92934ebbd7728756edf8
Jonas Holzman <jonas@holzman.fr>
2025-08-21 14:33:19 +0200

    iOS: Improve external file support
    Improve handling of external files, implements the iOS start/stop
    security scope resource access paradigms used for opening files, as
    well as iOS bundle property modifications to allow Blender to have it's
    own iOS Document folder (accessible from On My iPad > Blender in the
    Files app) and properly recognize file type.
    
    The Blender Document folder is also now accessible in the File Browser
    from the System shelf, removing the now outdated "Bundle Assets" folder.
    
    What works:
    - Opening / Saving files in Blender's own Document folder
    - Opening files from anywhere on the iPad (use the share to Blender
      action in the Files Browser)
    
    What doesn't work:
    - Saving files opened from outside Blender's own Document folder in
      place. To workaround this limitation, the Save operator is disabled
      for files, requiring the user to save the file
    - Opening files outside Blender's own Document folder from the Blender
      File Browser, the iOS Files app has to be used, in the near future,
      the iOS File Browser could be integrated directly in Blender to
      workaround such sandbox limitations.

--- intern/ghost/intern/GHOST_SystemIOS.hh
+++ intern/ghost/intern/GHOST_SystemIOS.hh
@@ -230,8 +230,8 @@ class GHOST_SystemIOS : public GHOST_System {
    * \param window: The window on which the event occurred.
    * \return Indication whether the event was handled.
    */
-  GHOST_TSuccess popupOnScreenKeyboard(GHOST_IWindow *window,
-                                       const GHOST_KeyboardProperties &keyboard_properties) override;
+  GHOST_TSuccess popupOnScreenKeyboard(
+      GHOST_IWindow *window, const GHOST_KeyboardProperties &keyboard_properties) override;
 
   /**
    * Hides a popup a keybaord. Called by GHOST_WindowIOS window subclass
@@ -243,6 +243,9 @@ class GHOST_SystemIOS : public GHOST_System {
   GHOST_TSuccess hideOnScreenKeyboard(GHOST_IWindow *window) override;
 
   const char *getKeyboardInput(GHOST_IWindow *window) override;
+
+  GHOST_TSuccess startSecurityScopedFileAccess(const char *filepath);
+  GHOST_TSuccess stopSecurityScopedFileAccess(const char *filepath);
 
   /**
    * Handles a window event. Called by GHOST_WindowIOS window delegate
