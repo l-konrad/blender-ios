e1673e17d45c5f4dffabc8a3332e2a1c72ef1ebc
salmazov <>
2026-02-28 12:23:09 +0100

    GPU: add null shader guards to all shader accessor functions
    All GPU_shader_get_* functions (get_uniform, get_constant, get_ssbo_binding,
    get_uniform_block, get_ubo_binding, get_sampler_binding, get_attribute,
    get_attribute_info, get_attribute_len, get_ssbo_input_len, get_builtin_uniform)
    now return -1/0/false when shader is NULL instead of crashing.
    
    This prevents EXC_BAD_ACCESS when EEVEE's draw passes try to bind resources
    (e.g. tilemaps_buf SSBO) to shaders that failed to compile on iOS Metal.
    The draw system already handles -1 binding slots gracefully.

--- source/blender/gpu/intern/gpu_shader.cc
+++ source/blender/gpu/intern/gpu_shader.cc
@@ -516,6 +516,9 @@ void GPU_shader_async_specialization_cancel(AsyncSpecializationHandle &handle)
 
 int GPU_shader_get_uniform(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *uniform = interface->uniform_get(name);
   return uniform ? uniform->location : -1;
@@ -523,6 +526,9 @@ int GPU_shader_get_uniform(gpu::Shader *shader, const char *name)
 
 int GPU_shader_get_constant(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *constant = interface->constant_get(name);
   return constant ? constant->location : -1;
@@ -530,12 +536,18 @@ int GPU_shader_get_constant(gpu::Shader *shader, const char *name)
 
 int GPU_shader_get_builtin_uniform(gpu::Shader *shader, int builtin)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   return interface->uniform_builtin(GPUUniformBuiltin(builtin));
 }
 
 int GPU_shader_get_ssbo_binding(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *ssbo = interface->ssbo_get(name);
   return ssbo ? ssbo->location : -1;
@@ -543,6 +555,9 @@ int GPU_shader_get_ssbo_binding(gpu::Shader *shader, const char *name)
 
 int GPU_shader_get_uniform_block(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *ubo = interface->ubo_get(name);
   return ubo ? ubo->location : -1;
@@ -550,6 +565,9 @@ int GPU_shader_get_uniform_block(gpu::Shader *shader, const char *name)
 
 int GPU_shader_get_ubo_binding(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *ubo = interface->ubo_get(name);
   return ubo ? ubo->binding : -1;
@@ -557,6 +575,9 @@ int GPU_shader_get_ubo_binding(gpu::Shader *shader, const char *name)
 
 int GPU_shader_get_sampler_binding(gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *tex = interface->uniform_get(name);
   return tex ? tex->binding : -1;
@@ -564,18 +585,27 @@ int GPU_shader_get_sampler_binding(gpu::Shader *shader, const char *name)
 
 uint GPU_shader_get_attribute_len(const gpu::Shader *shader)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return 0;
+  }
   const ShaderInterface *interface = shader->interface;
   return interface->valid_bindings_get(interface->inputs_, interface->attr_len_);
 }
 
 uint GPU_shader_get_ssbo_input_len(const gpu::Shader *shader)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return 0;
+  }
   const ShaderInterface *interface = shader->interface;
   return interface->ssbo_len_;
 }
 
 int GPU_shader_get_attribute(const gpu::Shader *shader, const char *name)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return -1;
+  }
   const ShaderInterface *interface = shader->interface;
   const ShaderInput *attr = interface->attr_get(name);
   return attr ? attr->location : -1;
@@ -586,6 +616,9 @@ bool GPU_shader_get_attribute_info(const gpu::Shader *shader,
                                    char r_name[256],
                                    int *r_type)
 {
+  if (UNLIKELY(shader == nullptr)) {
+    return false;
+  }
   const ShaderInterface *interface = shader->interface;
 
   const ShaderInput *attr = interface->attr_get(attr_location);
